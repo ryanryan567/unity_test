abaccd04addc89bf3c7b7fb4d38256e6f0c0bf03
chentaolu <chentaolu@mail.fcu.edu.tw>
2021-03-30 20:59:17 +0800

    add a server and create a floor

---
+++ Assets/Script/Server.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+namespace IOTServer
+{
+    class Server
+    {
+        private static byte[] result = new byte[1024];
+        private static int myPort = 8885;
+        static Socket serverSocket;
+
+        public Server()
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, myPort));
+            serverSocket.Listen(10);
+
+            Thread myThread = new Thread(ListenClientConnect);
+            myThread.Start();
+        }
+
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server Say Hello"));
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+        }
+    }
+}
